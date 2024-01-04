c5560a78b16a2eb12123f02f76e8ead55bfbf0df
amirakame <amirakamel343@gmail.com>
2024-01-04 14:29:49 -0800

    chool

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,14 +1,14 @@
 /* eslint-disable no-underscore-dangle */
 export default class HolbertonCourse {
 	  /**
-	   *    * Creates a new HolbertonCourse class
-	   *       *
-	   *          * @param {String} name - The name of the course.
-	   *             * @param {Number} length - How long the course is (in months).
-	   *                * @param {String[]} students - The names of students in the course.
-	   *                   *
-	   *                      * Implement getters and setters for each attribute
-	   *                         */
+	   * Creates a new HolbertonCourse class
+	   *
+	   * @param {String} name - The name of the course.
+	   * @param {Number} length - How long the course is (in months).
+	   * @param {String[]} students - The names of students in the course.
+	   *
+	   * Implement getters and setters for each attribute
+	   */
 	  constructor(name, length, students) {
 		      this.name = name;
 		      this.length = length;
@@ -27,15 +27,15 @@ export default class HolbertonCourse {
 		    }
 
 	  get length() {
-	      return this._length;
-	        }
+		      return this._length;
+		    }
 
-		  set length(value) {
+	  set length(value) {
 		      if (typeof value !== 'number') {
-		            throw new TypeError('Length must be a number');
-			        }
-    this._length = value;
-  }
+			            throw new TypeError('Length must be a number');
+			          }
+		      this._length = value;
+		    }
 
 	  get students() {
 		      return this._students;
