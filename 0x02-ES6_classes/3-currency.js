b891e9dd2bc77dad15dfc9e896c2345a47ff2bdf
amirakame <amirakamel343@gmail.com>
2024-01-04 14:55:26 -0800

    chool

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -1,30 +1,25 @@
 export default class Currency {
-	  constructor(code, name) {
-		      this.code = code;
-		      this.name = name;
-		    }
+  constructor(code, name) {
+    this.code = code;
+    this.name = name;
+  }
 
-	  get code() {
-		      return this._code;
-		    }
+  get code() {
+    return this._code;
+  }
 
-	  set code(value) {
-		      this._code = value;
-		    }
+  set code(value) {
+    this._code = value;
+ }
 
-	  get name() {
-		      return this._name;
-		    }
+ get name() {
+   return this._name;
+ }
 
-	  set name(value) {
-		      this._name = value;
-		    }
-
-	  /**
-	   *    * Creates full string representation of this Currency.
-	   *       * @returns {String}
-	   *          */
-	  displayFullCurrency() {
-		      return `${this.name} (${this.code})`;
-		    }
+ set name(value) {
+   this._name = value;
+ }
+ displayFullCurrency() {
+   return `${this.name} (${this.code})`;
+ }
 }
